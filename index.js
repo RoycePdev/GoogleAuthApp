c769badba78181d3a20029889767c4638399a605
Brian <bschaferwebdev619@gmail.com>
2021-10-25 16:57:36 -0700

    Finished google auth

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require("passport")
+require('./models/User')
 require('./services/passport')
+const mongoose = require('mongoose')
 
+mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 
